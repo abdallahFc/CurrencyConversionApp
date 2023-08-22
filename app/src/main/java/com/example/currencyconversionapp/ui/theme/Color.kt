1f7be85cc37e4f9a3ca97c622918dcdb7a4b6d8b
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-08-22 16:42:10 +0300

    Initial commit

---
+++ app/src/main/java/com/example/currencyconversionapp/ui/theme/Color.kt
@@ -0,0 +1,21 @@
+package com.example.currencyconversionapp.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// Text colors
+val Black = Color(0xFF000000)
+val DarkGrey = Color(0xFF202020)
+val White = Color(0xFFFFFFFF)
+
+// Custom colors
+val ButtonColor = Color(0xFF3CACFE)
+val FieldColor = Color(0xFFF9F9F9)
+val FieldShadowColor = Color(0xFFC5C5C5)
