6a8eb3335123b7e0392af2dabba127ea8480cbfe
Abdallah Mohamed <abdallah.mohamed2274@gmail.com>
2023-08-29 16:39:23 +0300

    convert finish

--- app/src/main/java/com/example/currencyconversionapp/presentation/feature/conversion/ConvertUiState.kt
+++ app/src/main/java/com/example/currencyconversionapp/presentation/feature/conversion/ConvertUiState.kt
@@ -3,8 +3,8 @@ package com.example.currencyconversionapp.presentation.feature.conversion
 data class ConvertUiState(
     val isLoading: Boolean = false,
     val isLoadingList: Boolean = false,
-    val baseCurrency: CurrencyCode = CurrencyCode.EGP,
-    val targetCurrency: CurrencyCode = CurrencyCode.USD,
+    val baseCurrency: CurrencyCode = CurrencyCode.USD,
+    val targetCurrency: CurrencyCode = CurrencyCode.EGP,
     val amount: Double = 1.0,
     val convertedAmount: String = "",
     val currencies: List<CurrencyUiModel> = emptyList(),
@@ -20,14 +20,15 @@ data class CurrencyUiModel(
 )
 
 enum class CurrencyCode(val code: String) {
-    USD("USD"),
-    EUR("EUR"),
+    JPY("JPY"),
+    OMR("OMR"),
     GBP("GBP"),
+    USD("USD"),
+    SAR("SAR"),
+    KWD("KWD"),
+    EGP("EGP"),
+    QAR("QAR"),
     AED("AED"),
     BHD("BHD"),
-    JPY("JPY"),
-    KWD("KWD"),
-    OMR("OMR"),
-    SAR("SAR"),
-    EGP("EGP")
+    EUR("EUR")
 }
